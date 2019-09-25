1d4257db1ea52b3e3d8b892e3b686f6bc4028ee0
Robert Brisita <rbrisita@hotmail.com>
2019-09-25 09:04:13 -0400

    Adding command line client to test PLQ API service. Update README.md.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Simple Python Service in a docker container.
 docker-compose up -d
+
+Visit Docker's IP with Port to view index page to test Pig Lating Query.
+
+# Testing Client
+virtualenv venv
+source venv/bin/activate
+pip install -r cli_requirements.txt
+
+python3 cli.py <ip:port> <pig_latin_query>
---
+++ cli.py
@@ -0,0 +1,26 @@
+import sys
+import requests
+import urllib.parse
+'''
+Command line client to test against Pig Latin Query API service.
+'''
+
+# Check command line arguments
+if len(sys.argv) != 3:
+    print(f"Usage: {sys.argv[0]} <ip:port> <pig_latin_query>")
+    sys.exit(1)
+
+host = sys.argv[1]
+plq = sys.argv[2]
+
+# Prepare URL request
+url = f"http://{host}/api/plq/{urllib.parse.quote_plus(plq)}"
+print(url)
+
+response = requests.get(url)
+if response:
+    obj = response.json()
+    obj = obj['data']
+    print(f"{obj['plq']} = {obj['result']}")
+else:
+    print('An error has occurred. Please try again.')
